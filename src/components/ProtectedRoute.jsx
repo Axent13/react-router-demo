90a52dfaf31aa8f57162b5949672ffa1b8507af9
Axent13 <axent13@yandex.ru>
2023-02-14 07:56:15 +0700

    9. Программная навигация

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,27 +3,27 @@ import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { isLoggedInSelector } from "../store/authSlice";
 function ProtectedRoute({ component: Component, children, ...rest }) {
-    const isLoggedIn = useSelector(isLoggedInSelector());
+  const isLoggedIn = useSelector(isLoggedInSelector());
 
-    return (
-        <Route
-            {...rest}
-            render={(props) => {
-                if (!isLoggedIn) {
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: "/auth/login",
-                                state: {
-                                    referrer: props.location,
-                                },
-                            }}
-                        />
-                    );
-                }
-                return Component ? <Component {...props} /> : children;
-            }}
-        />
-    );
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!isLoggedIn) {
+          return (
+            <Redirect
+              to={{
+                pathname: "/auth/login",
+                state: {
+                  referrer: props.location,
+                },
+              }}
+            />
+          );
+        }
+        return Component ? <Component {...props} /> : children;
+      }}
+    />
+  );
 }
 export default ProtectedRoute;
